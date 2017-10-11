7f1cd1823dc4d63814bec2123d783bf3feae238e
pjwilliams2 <patrick@libboo.com>
2017-10-11 09:03:03 -0400

    Started problem #101

---
+++ 101/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Euler101
+{
+    //https://projecteuler.net/problem=101
+    //un = 1 − n + n2 − n3 + n4 − n5 + n6 − n7 + n8 − n9 + n10
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coeffs = new int[]{1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1};
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
