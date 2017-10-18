2a954b3d5999d6f4e4ea5bfc84dc26930da4c4dc
pjwilliams2 <patrick@libboo.com>
2017-10-17 23:43:07 -0400

    Solution to problem 7

---
+++ 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Euler7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxOrdinalIndex = 10_001;
+            var ordinalIndex = 0;
+            var primeNum = 0D;
+
+            for(double num = 2; ordinalIndex < maxOrdinalIndex; num++)
+                if(Euler.MathExt.IsPrime(num))
+                {
+                    ordinalIndex++;
+                    primeNum = num;
+                }
+
+            Console.WriteLine("The {0} prime is: {1}", ordinalIndex, primeNum);
+        }
+    }
+}
