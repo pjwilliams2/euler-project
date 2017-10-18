a58fdbfe505a090f89064ca52de89be28658d7e0
pjwilliams2 <patrick@libboo.com>
2017-10-18 11:51:24 -0400

    Added coordinate parsing method

--- 102/Program.cs
+++ 102/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Euler102
 {
@@ -6,7 +7,35 @@ namespace Euler102
     {
         static void Main(string[] args)
         {
+            var rdr = new StreamReader("p102_triangles.txt");
+            
+            TestParseTriangleCoords();
+
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        static int[][] ParseTriangleCoords(string str)
+        {
+            //input should be in the format: x1,y1,x2,y2,x3,y3
+            int[][] arr = new int[3][];
+
+            var strs = str.Split(',', StringSplitOptions.None);
+
+            for(int i = 0; i < 3; i++)
+                arr[i] = new int[2]{ Int32.Parse(strs[2*i]), Int32.Parse(strs[2*i+1]) };
+
+            return arr;
+        }
+
+        static void TestParseTriangleCoords()
+        {
+            var str = "4,3,-9,0,6,-1";
+            var arr = ParseTriangleCoords(str);
+
+            foreach(var pair in arr)
+                Console.WriteLine("x: {0}, y: {1}", pair[0], pair[1]);
         }
     }
 }
