c77ac1fde0ae434e0682db59468a88f872b285a7
pjwilliams2 <patrick@libboo.com>
2017-10-17 23:06:06 -0400

    Created solution to problem 6

---
+++ 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Euler6
+{
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumOfSquares = 0;
+            double squareOfSums = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += i * i;
+                squareOfSums += i;
+            }
+
+            //square the sums
+            squareOfSums *= squareOfSums;
+
+            Console.WriteLine("{0} - {1} = {2}", squareOfSums, sumOfSquares, squareOfSums - sumOfSquares);
+        }
+    }
+}
