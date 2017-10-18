9714c19533582715e3f2beded7085cb9cefda38a
pjwilliams2 <patrick@libboo.com>
2017-10-17 23:31:54 -0400

    Created helper library for common operations

---
+++ Utilities/MathExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Euler
+{
+    public static class MathExt
+    {
+        public static bool IsPrime(double number)
+        {
+            for(long i = 2; i <= Math.Sqrt(number); i++)
+                if(number % i == 0)
+                    return false;
+            
+            return true;
+        }
+    }
+}
