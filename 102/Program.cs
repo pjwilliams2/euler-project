66961ea8a86c335b3997f5ad22386c5fd84a86c3
pjwilliams2 <patrick@libboo.com>
2017-10-18 23:50:57 -0400

    Solved problem 102

--- 102/Program.cs
+++ 102/Program.cs
@@ -8,23 +8,44 @@ namespace Euler102
         static void Main(string[] args)
         {
             var rdr = new StreamReader("p102_triangles.txt");
-            
-            TestParseTriangleCoords();
+            var count = 0;
 
+            while(!rdr.EndOfStream)
+            {
+                var coords = ParseTriangleCoords(rdr.ReadLine());
+                double[] pt_a = coords[0], pt_b = coords[1], pt_c = coords[2];
 
+                //make sure the x coordinate of the angle bisector and the opposite line segment are between the x's of the line segment
+                // A bisect BC x coord
+                // Solving for: Ax / Ay * X = (Cy - By) / (Cx - Bx) * (X - Bx) + By
+                double x1 = ((pt_b[1] - pt_c[1]) / (pt_c[0] - pt_b[0]) * pt_b[0] + pt_b[1]) / (pt_a[1] / pt_a[0] + (pt_c[1] - pt_b[1]) / (pt_b[0] - pt_c[0]));
+                // B bisect AC x coord
+                double x2 = ((pt_a[1] - pt_c[1]) / (pt_c[0] - pt_a[0]) * pt_a[0] + pt_a[1]) / (pt_b[1] / pt_b[0] + (pt_c[1] - pt_a[1]) / (pt_a[0] - pt_c[0]));
+                // C bisect AB x coord
+                double x3 = ((pt_a[1] - pt_b[1]) / (pt_b[0] - pt_a[0]) * pt_a[0] + pt_a[1]) / (pt_c[1] / pt_c[0] + (pt_b[1] - pt_a[1]) / (pt_a[0] - pt_b[0]));
 
-            Console.WriteLine("Hello World!");
+                if((x1 < pt_b[0] && x1 < pt_c[0]) || (x1 > pt_b[0] && x1 > pt_c[0]) ||
+                   (x2 < pt_a[0] && x2 < pt_c[0]) || (x2 > pt_a[0] && x2 > pt_c[0]) ||
+                   (x3 < pt_a[0] && x3 < pt_b[0]) || (x3 > pt_a[0] && x3 > pt_b[0]))
+                {
+                    continue;
+                }
+
+                count++;
+            }
+
+            Console.WriteLine("Number of triangles with origin: " + count);
         }
 
-        static int[][] ParseTriangleCoords(string str)
+        static double[][] ParseTriangleCoords(string str)
         {
             //input should be in the format: x1,y1,x2,y2,x3,y3
-            int[][] arr = new int[3][];
+            double[][] arr = new double[3][];
 
             var strs = str.Split(',', StringSplitOptions.None);
 
             for(int i = 0; i < 3; i++)
-                arr[i] = new int[2]{ Int32.Parse(strs[2*i]), Int32.Parse(strs[2*i+1]) };
+                arr[i] = new double[2]{ Int32.Parse(strs[2*i]), Int32.Parse(strs[2*i+1]) };
 
             return arr;
         }
